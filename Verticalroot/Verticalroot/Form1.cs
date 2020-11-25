bf4bd88a8f79c05fb15554e54d9914de7f8eea54
Sander van Deurzen <D251669@edu.rocwb.nl>
2020-11-25 13:36:37 +0100

    Database connection
    display in the making

--- Verticalroot/Verticalroot/Form1.cs
+++ Verticalroot/Verticalroot/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Verticalroot
 {
@@ -15,6 +16,31 @@ namespace Verticalroot
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "datasource=localhost;port=3306;username=root;password="; //database server name and mysql port and username and password
+                string mysql = "SELECT * FROM vertical_root.users;";
+
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                MySqlCommand command = new MySqlCommand(mysql, conn);
+
+                conn.Open();
+
+                MySqlDataAdapter dtb = new MySqlDataAdapter();
+                dtb.SelectCommand = command;
+
+                DataTable dtable = new DataTable();
+                dtb.Fill(dtable);
+                dataGridView1.DataSource = dtb;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
