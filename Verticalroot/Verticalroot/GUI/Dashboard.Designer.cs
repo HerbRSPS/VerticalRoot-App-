d37db691089760c47fbb993328942261b662962c
Meradj <meradj2000@hotmail.com>
2020-11-25 10:58:35 +0100

    Dashboard
    GUI

--- Verticalroot/Verticalroot/GUI/Dashboard.Designer.cs
+++ Verticalroot/Verticalroot/GUI/Dashboard.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Verticalroot
+﻿namespace Verticalroot.GUI
 {
     partial class Dashboard
     {
--- Verticalroot/Verticalroot/GUI/Dashboard.cs
+++ Verticalroot/Verticalroot/GUI/Dashboard.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Verticalroot
+namespace Verticalroot.GUI
 {
     public partial class Dashboard : Form
     {
