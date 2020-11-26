97955c2378ee81d351733495d3cbfdab98cd8716
Arend Evers <arendevers@hotmail.nl>
2020-11-26 11:29:28 +0100

    rounded button
    yes

--- Verticalroot/VerticalRoot/VerticalRoot/MainWindow.xaml.cs
+++ Verticalroot/VerticalRoot/VerticalRoot/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace VerticalRoot
         {
             InitializeComponent();
         }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
