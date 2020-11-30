b15af54282ebdef50245603543135213af1c368f
Arend Evers <arendevers@hotmail.nl>
2020-11-30 10:04:51 +0100

    wa ik nu heb
    bfwbiewfbi

--- Verticalroot/VerticalRoot/VerticalRoot/MainWindow.xaml.cs
+++ Verticalroot/VerticalRoot/VerticalRoot/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace VerticalRoot
 {
@@ -31,7 +32,29 @@ namespace VerticalRoot
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("test");
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Fontys\Proftaak\GroepsProjectArduino\Git Kraken\C#\VerticalRoot-App-\Verticalroot\VerticalRoot\VerticalRoot\Database voor inlog\inlogDB.mdf;Integrated Security=True;Connect Timeout=30");
+            String query = "Select * from tbl_login Where username = '" + tbUsername.Text.Trim() + "' and password = '" + tbPassword.Text.Trim()+"'";
+                    //query = "SELECT * From TableName WHERE Title = @Title";
+                    //command.Parameters.Add("@Title", SqlDbType.VarChar).Value = someone;
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            System.Data.DataTable dtbl = new System.Data.DataTable();
+            sda.Fill(dtbl);
+            if (dtbl.Rows.Count == 1)
+            {
+                frmDashboard objfrmDashboard = new frmDashboard();
+                this.Hide();
+                objfrmDashboard.Show();
+            }
+            else
+            {
+                MessageBox.Show("Foutieve inloggegevens");
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
